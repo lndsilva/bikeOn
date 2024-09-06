83ad0b08e14f56663a89432033b87f9c6d1e0ce3
lndsilva <lndsilva@hotmail.com>
2024-09-05 22:13:26 -0300

    abrindo de login

--- app/src/main/java/br/com/etec/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etec/myapp/LoginActivity.java
@@ -1,6 +1,8 @@
 package br.com.etec.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class LoginActivity extends AppCompatActivity {
+    MaterialButton abrir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,15 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        abrir = findViewById(R.id.btnEntrar);
+
+        abrir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),SplashActivity.class));
+                finish();
+            }
         });
     }
 }
