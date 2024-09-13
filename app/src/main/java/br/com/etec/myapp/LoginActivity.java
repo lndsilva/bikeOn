0cd2b03f761752b18223f0c0416eb9e9595cd32c
lndsilva <lndsilva@hotmail.com>
2024-09-12 22:08:08 -0300

    ações dos botões

--- app/src/main/java/br/com/etec/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etec/myapp/LoginActivity.java
@@ -3,6 +3,7 @@ package br.com.etec.myapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +12,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 
 public class LoginActivity extends AppCompatActivity {
     //declarar as váriaveis globais
     MaterialButton btnRecuperarSenha, btnCadastrarSenha, btnEntrar;
+    TextInputEditText txtSenha, txtEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +36,31 @@ public class LoginActivity extends AppCompatActivity {
         btnCadastrarSenha = findViewById(R.id.btnCadastrarSenha);
         btnRecuperarSenha = findViewById(R.id.btnRecuperarSenha);
         btnEntrar = findViewById(R.id.btnEntrar);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtSenha = findViewById(R.id.txtSenha);
 
         //criando as ações dos botões
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password;
+
+                email = txtEmail.getText().toString().trim();
+                password = txtSenha.getText().toString().trim();
+
+                if (email.equals("etecia") && password.equals("etecia")) {
+                    startActivity(new Intent(getApplicationContext(),
+                            MenuPrincipalActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Usuário ou senha inválidos",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
         btnCadastrarSenha.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,7 +72,7 @@ public class LoginActivity extends AppCompatActivity {
         btnRecuperarSenha.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),RecuperarSenhaActivity.class));
+                startActivity(new Intent(getApplicationContext(), RecuperarSenhaActivity.class));
                 finish();
             }
         });
