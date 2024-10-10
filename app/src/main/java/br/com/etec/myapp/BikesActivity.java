8fa8a76514b1fa086c965ffa4dd0b4970805cc87
lndsilva <lndsilva@hotmail.com>
2024-10-10 19:34:35 -0300

    toolbar menu

--- app/src/main/java/br/com/etec/myapp/BikesActivity.java
+++ app/src/main/java/br/com/etec/myapp/BikesActivity.java
@@ -1,6 +1,8 @@
 package br.com.etec.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +11,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.tabs.TabLayout;
 
 public class BikesActivity extends AppCompatActivity {
     TabLayout idTabLayoutBikes;
     ViewPager2 idViewPagerBikes;
     AdapterViewPagerBikes adapterViewPagerBikes;
+    MaterialToolbar idToolBarBike;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,18 @@ public class BikesActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        idToolBarBike = findViewById(R.id.idToolBarBike);
+
+        //clicar no botão voltar
+        idToolBarBike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),
+                        MenuPrincipalActivity.class));
+                finish();
+            }
         });
 
         idTabLayoutBikes = findViewById(R.id.idTabLayoutBikes);
